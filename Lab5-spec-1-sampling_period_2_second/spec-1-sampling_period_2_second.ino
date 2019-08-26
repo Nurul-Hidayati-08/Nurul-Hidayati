2a84416891fc2d5b8029b2b50ece4ba39735b536
Nurul-Hidayati-08 <54526365+Nurul-Hidayati-08@users.noreply.github.com>
2019-08-26 16:08:27 +0900

    Add files via upload

---
+++ Lab5-spec-1-sampling_period_2_second/spec-1-sampling_period_2_second.ino
@@ -0,0 +1,50 @@
+#include <OneWire.h>  
+
+
+OneWire ds(2); 
+int period = 2000;
+unsigned long time_now = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  byte i;
+  byte present = 0;
+
+  byte data[12];
+  byte addr[8];
+  float Temp;
+
+  time_now = millis();
+
+  while(millis() < time_now + period){
+    }
+  
+  if (!ds.search(addr)) {
+    ds.reset_search();
+    return;
+  }
+
+ds.reset();
+  ds.select(addr);
+  ds.write(0x44,1); // start conversion, with parasite power on at the end
+  //delay(2000);
+
+  present = ds.reset();
+  ds.select(addr);
+  ds.write(0xBE); // Read all data of Scratchpad
+
+  for (i = 0; i < 9; i++) 
+     data[i] = ds.read(); {
+  }
+
+  Temp=(data[1]<<8)+data[0]; // calculate the Celsius.
+  Temp=Temp/16;
+  
+  Serial.print("\nC="); // Celsius output
+  Serial.print(Temp);
+  Serial.print(", ");
+  
+}
